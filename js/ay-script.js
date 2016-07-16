be87f7b5654a746dab17d92d577560b3dbf694de
vamsi.k <vamsi.k@USER-PC.(none)>
2016-07-17 01:02:28 +0530

    Adding again

---
+++ js/ay-script.js
@@ -0,0 +1,18 @@
+/*function toggleLeftMenu(){
+	if($('#leftMenu').hasClass('collapsed')){
+		$('#leftMenu').removeClass('collapsed');
+	}else{
+		$('#leftMenu').addClass('collapsed');
+	}
+}*/
+/*tootip start*/
+/*$('[data-toggle=tooltip]').hover(function(){
+	var showLabels = $('#leftMenu').hasClass('collapsed');
+	console.log(showLabels);
+	if(showLabels){
+		$(this).tooltip('show');
+	}
+}, function(){
+	$(this).tooltip('hide');
+});*/
+/*tootip end*/
